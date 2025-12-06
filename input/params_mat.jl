7178732535989b3362e7ed1d07b2820c6bc320d0
st188719 <st188719@stud.uni-stuttgart.de>
2025-12-06 17:05:20 +0100

    changed all material parameters to equal to 1

--- input/params_mat.jl
+++ input/params_mat.jl
@@ -1,9 +1,9 @@
 # Material parameters (transversely isotropic)
 λ     = 1.0
-μ_l   = 10.0
+μ_l   = 1.0
 μ_t   = 1.0
-alpha = 5.0
-beta  = 2.0
+alpha = 1.0
+beta  = 1.0
 angle = 0.0  # Default fiber angle (can be overridden by multi_angle_topopt.jl)
 
 # Select which material properties are treated as spatial random fields via KL expansion.
